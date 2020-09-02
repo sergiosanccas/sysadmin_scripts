9e6fd9c8d3cdaa4665333dceff1628e5d19f506c
Sergio SC <sergio.tendi@gmail.com>
2020-09-02 16:25:02 +0200

    cambiamos los mensjaes

--- install-docker.sh
+++ install-docker.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+banner()
+{
+  echo "+------------------------------------------+"
+  printf "| %-40s |\n" "`date`"
+  echo "|                                          |"
+  printf "|`tput bold` %-40s `tput sgr0`|\n" "$@"
+  echo "+------------------------------------------+"
+}
+
 echo -e "Ejecutammos el script con el usuario: \e[32m $USER \033[0m"
 
 echo -e " \e[34m------------------- Actualizamos el sistema --------------- \033[0m"
@@ -8,15 +17,17 @@ sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent softwa
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
-echo "Anadimos la key"
+banner "Anadimos la key"
 sudo apt-key fingerprint 0EBFCD88
 
 
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 
-echo "Actualizamos e instalamos docker"
+banner "Actualizamos e instalamos docker"
 sudo apt-get update -y
 sudo apt-get install docker-ce docker-ce-cli containerd.io
 
-echo "Anadimos el usuario $USER al grupo"
+banner "Anadimos el usuario $USER al grupo"
 sudo usermod -aG docker $USER
+
+banner "Terminamos la ejecucion"
